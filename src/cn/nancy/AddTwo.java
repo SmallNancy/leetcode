976ceeeb6b3896f29edeec85ab7eacb7056b3759
smallnancy <smallnancy@outlook.com>
2019-03-19 12:23:11 +0800

    运用三目运算符解决

--- src/cn/nancy/AddTwo.java
+++ src/cn/nancy/AddTwo.java
@@ -1,5 +1,16 @@
 package cn.nancy;
 
+/**
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit.
+ * Add the two numbers and return it as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *
+ * Example:
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ */
 public class AddTwo {
     public static void main(String[] args) {
         ListNode l1 = new ListNode(2);
@@ -8,28 +19,27 @@ public class AddTwo {
         ListNode l2 = new ListNode(5);
         l2.next = new ListNode(6);
         l2.next.next = new ListNode(4);
+        System.out.println(l1);
+        System.out.println(l2);
 
         System.out.println(addTwoNumbers(l1,l2));
 
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode newlist = null;
-        ListNode temp = newlist;
-        int val = 0;
-        while(l1.next != null || l2.next != null){
-            val += l1.val +l2.val;
-            if(val > 10){
-                temp = new ListNode(0);
-                val = 1;
-            }else {
-                temp = new ListNode(val);
-                val = 0;
-            }
-            temp = temp.next;
+        ListNode ln1 = l1,ln2 = l2,head = null,node = null;
+        int carry = 0,contain = 0,sum = 0;
+        head = node = new ListNode(0);
+        while(ln1 != null || ln2 != null || carry != 0){
+            sum = (ln1!= null ?ln1.val:0) + (ln2 != null ?ln2.val:0) + carry;
+            carry = sum/10;
+            contain = sum % 10;
+            node = node.next = new ListNode(contain);
+            ln1 = (ln1 != null ? ln1.next : null);
+            ln2 = (ln2 != null ? ln2.next : null);
         }
-        return newlist;
 
+        return  head.next;
     }
 
 }
@@ -45,6 +55,8 @@ class ListNode {
     public String toString() {
         return "ListNode{" +
                 "val=" + val +
+                ", next=" + next +
                 '}';
     }
+
 }
