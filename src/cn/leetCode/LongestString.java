6dbe57fe1c582439d5d2bfa80c506a3ae17ffced
smallnancy <smallnancy@outlook.com>
2019-03-21 10:50:56 +0800

    longest substring

--- src/cn/leetCode/LongestString.java
+++ src/cn/leetCode/LongestString.java
@@ -1,5 +1,8 @@
 package cn.leetCode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * leetCode 02
  * Given a string, find the length of the longest substring without repeating characters.
@@ -17,14 +20,24 @@ package cn.leetCode;
  */
 public class LongestString {
     public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
 
     }
 
-
-
     // TODO 最长不相同子串
-    public int lengthOfLongestSubstring(String s) {
-        return 0;
-
+    public static int lengthOfLongestSubstring(String s) {
+        int i = 0,j = 0,max = 0;
+        Set<Character> set = new HashSet<>();
+        while(j < s.length()){
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                max = Math.max(max,set.size()); // 保留最大长度
+            }else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        //max = set.size();
+        return max;
     }
 }
