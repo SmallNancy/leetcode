e2dbb52b755097ec95e09e6288dffdbffe832a9b
smallnancy <smallnancy@outlook.com>
2019-03-21 12:06:00 +0800

    reConstruct binary tree

--- src/cn/swordPointOffer/ReBinaryTree.java
+++ src/cn/swordPointOffer/ReBinaryTree.java
@@ -1,5 +1,7 @@
 package cn.swordPointOffer;
 
+import java.util.Scanner;
+
 /**
  * @Author: Nancy
  * @Date: 2019/3/19 21:14
@@ -10,13 +12,30 @@ package cn.swordPointOffer;
  */
 public class ReBinaryTree {
     public static void main(String[] args) {
-
+        int[] pre = {3,9,20,15,7};
+        int[] in = {9,3,15,20,7};
+        System.out.println(reConstructBinaryTree(pre,in));
     }
 
-    public static TreeNode reConstructBinaryTree(int [] pre,int [] in) {
-        return null;
-
+    public static TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        TreeNode root = rebuildTree(pre,0,pre.length - 1,in,0,in.length -1);
+        return root;
     }
+
+    public static TreeNode rebuildTree(int[] pre, int startpre, int endpre, int[] in, int startin, int endin) {
+        if (startpre > endpre || startin > endin)
+            return null;
+        TreeNode root = new TreeNode(pre[startpre]);
+        for (int i = startin;i < in.length; i++){
+            if (in[i] == pre[startpre]){
+                root.left = rebuildTree(pre, startpre  + 1, startpre + i - startin, in, startin, i - 1);
+                root.right = rebuildTree(pre, startpre  + i - startin + 1, endpre, in, i + 1, endin);
+                break;
+            }
+        }
+        return root;
+    }
+
 
 }
 class TreeNode {
@@ -25,5 +44,14 @@ class TreeNode {
     TreeNode right;
     TreeNode(int x) {
         val = x;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
     }
 }
